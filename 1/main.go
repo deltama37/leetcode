ed48916c817d3ea1f2c9502fca2318ac0499f5a5
ryota misumi <3delta7@gmail.com>
2020-01-12 03:51:46 +0900

    1

---
+++ 1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Println(twoSum(nums, target))
+}
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
+		}
+		m[v] = i
+	}
+	return []int{-1, -1}
+}
